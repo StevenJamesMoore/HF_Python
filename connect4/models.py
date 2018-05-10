7570f36095f595dcbb504cfe027828080e001915
Interactive Data Science <InteractiveDataScience@gmail.com>
2018-05-10 00:35:18 -0400

    Playing around with initial state setup, should probably use a library for my 2D array board state.

--- connect4/models.py
+++ connect4/models.py
@@ -1,26 +1,47 @@
+import copy
+
+from django.core.validators import validate_comma_separated_integer_list
 from django.db import models
 import datetime
 from django.utils import timezone
 
+
 # Create your models here.
 
+class State(models.Model):
+    board = models.TextField(max_length = 100)
+    actions = models.CharField(validators=[validate_comma_separated_integer_list], max_length=30, blank=True, null=True)
+
+    def get_state(self):
+        return copy.deepcopy(self.board)
+
+    def add_piece(self):
+        self.board
+
+    def __init__(self, board):
+        self.board = board
+
+    def __str__(self):
+        return self.board
+
+
 class Question(models.Model):
-   question_text = models.CharField(max_length=200)
-   pub_date = models.DateTimeField('date published')
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
 
-   def __str__(self):
-       return self.question_text
+    def __str__(self):
+        return self.question_text
 
-   def was_published_recently(self):
-       now = timezone.now()
-       return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-   was_published_recently.admin_order_field = 'pub_date'
-   was_published_recently.boolean = True
-   was_published_recently.short_description = 'Published recently?'
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.DO_NOTHING,)
+    question = models.ForeignKey(Question, on_delete=models.DO_NOTHING, )
     choice_test = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
--- connect4/views.py
+++ connect4/views.py
@@ -1,13 +1,39 @@
+from typing import List, Any
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-
-from .models import Question, Choice
+import csv
+import os.path
+from .models import Question, Choice, State
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+
+    my_path = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(my_path, "../data/datasetone.csv")
+
+    blank_board = [[0 for x in range(7)] for y in range(6)]
+    start = State(blank_board)
+    current_state = start
+
+    all_states = []
+    with open(path) as f:
+        test_list: List[Any] = list(csv.reader(f))
+        for x in test_list:
+            starting_player = x[0]
+            winning_player = x[1]
+            for item in x[2:]:
+                board = current_state.get_state()
+                #item is '5,4'
+                board[int(item.split(',')[0])][int(item.split(',')[1])] = 2
+                s = State(board)
+                current_state = s
+                all_states.append(s)
+                breakPoint = "here"
+
+    context = {'latest_question_list': latest_question_list,
+               'test_list': test_list}
     return render(request, 'connect4/index.html', context)
 
 
