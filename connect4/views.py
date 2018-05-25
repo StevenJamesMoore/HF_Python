5953dde1ba7665f742bb698b7e6fdf56db0e2640
Interactive Data Science <InteractiveDataScience@gmail.com>
2018-05-24 23:42:39 -0400

    Cleaned up the code and removed some older comments.

--- connect4/views.py
+++ connect4/views.py
@@ -16,106 +16,78 @@ def index(request):
     path = os.path.join(my_path, "../data/datasetone.csv")
 
     blank_board = [[0 for x in range(7)] for y in range(6)]
-    start = State(blank_board, 0)
-
     start_state = State(blank_board, 0)
-    x = copy.deepcopy(start_state.board)
-    x[2][3] = 4
-    following_state = State(x, 1)
-    one = start_state.board
-    two = following_state.board
-    following_state.add_action(4)
-    following_state.add_transition(4)
-    following_state.add_next_state(start_state)
-    what_are_the_next_states = following_state.next_states
-    next_trans = following_state.transitions
-    next_acts = following_state.actions
-    somethingelse = following_state.reward = 5
-    the_reward = following_state.reward
 
-    one_last_breakpoint = "many more to come"
-
+    # Make a dictionary, create the first entry to be the blank (starting) board state.
+    # Keys are string representations of the 2D array representing the board, since you can't have a 2D arr as a key
     all_states = ct.defaultdict()
     all_states[repr(blank_board)] = start_state
     state_id = 0
     with open(path) as f:
-        test_list: List[Any] = list(csv.reader(f))
-        for x in test_list:
+        data_rows: List[Any] = list(csv.reader(f))
+        for x in data_rows:
             current_state = start_state
             starting_player = x[0]
             winning_player = x[1]
-            won = starting_player == winning_player
 
-            i = 3
+            if winning_player == 3:
+                won = 3  # Tie game
+            elif winning_player == starting_player:
+                won = 1  # Starting player won
+            else:
+                won = -1  # Starting player lost
+
+            i = 3  # Start i at 3 since the first two columns are who started and won. The third column is x from below.
             for item in x[2::2]:
                 state_id = state_id + 1
-                if not item:
+                if not item:  # Games have a variable number of turns, with at most 42, so there will be blank values.
                     break
-                elif not x[i]:
-                    board = copy.deepcopy(current_state.board)
-                    player_x = int(item.split(',')[0])
-                    player_y = int(item.split(',')[1])
-                    board[player_x][player_y] = 1
-
-                    if repr(board) in all_states:
-                        new_state = all_states[repr(board)]
-                        # Check to see if it's already been linked from current_state
-                        if current_state.next_states.__contains__(new_state):
-                            # We need to modify the transition accordingly
-                            ind = current_state.next_states.index(new_state)
-                            current_state.transitions[ind] = current_state.transitions[ind] + 1
-                            breakItDownHere = "Wow, that state exists and has already been linked"
-                        else:
-                            current_state.add_next_state(new_state)
-                            current_state.add_action(player_y)
-                            current_state.add_transition(1)
-                    else:
-                        new_state = State(board, state_id)
-                        new_state.reward = 1
-                        current_state.add_next_state(new_state)
-                        current_state.add_action(player_y)
-                        current_state.add_transition(1)
-                        all_states[repr(new_state.board)] = new_state
                 else:
                     board = copy.deepcopy(current_state.board)
-                    player_x = int(item.split(',')[0])
-                    player_y = int(item.split(',')[1])
-                    board[player_x][player_y] = 1
-                    opponent_x = int(x[i].split(',')[0])
-                    opponent_y = int(x[i].split(',')[1])
-                    board[opponent_x][opponent_y] = 2
-
-                    # If this board state already exists in our model, find it and make new_state it!
-                    # Point our current_state to it, modify current_state's transition + action accordingly.
-                    # Make our current_state this existing state (assigned to new_state) and continue.
-                    if repr(board) in all_states:
-                        new_state = all_states[repr(board)]
-                        # Check to see if it's already been linked from current_state
-                        if current_state.next_states.__contains__(new_state):
-                            # We need to modify the transition accordingly
-                            ind = current_state.next_states.index(new_state)
-                            current_state.transitions[ind] = current_state.transitions[ind] + 1
-                            breakItDownHere = "Wow, that state exists and has already been linked"
-                        else:
-                            current_state.add_next_state(new_state)
-                            current_state.add_action(player_y)
-                            current_state.add_transition(1)
-                    else:
-                        new_state = State(board, state_id)
-                        current_state.add_next_state(new_state)
-                        current_state.add_action(player_y)
-                        current_state.add_transition(1)
-                        all_states[repr(new_state.board)] = new_state
-
-                i = i + 2
-                current_state = new_state
-                breakPoint = "here"
-
-            doneWithThefor = "wow"
+                    add_piece_to_board(board, item, 1)
+                    action = int(item.split(',')[1])
+                    if x[i]:
+                        add_piece_to_board(board, x[i], 2)
+                    current_state = existing_board_state(board, current_state, all_states, action, state_id)
+                i = i + 2  # Increment by 2 since x is the players move and i is getting the following (opp) move
+            # END: for item in x[2::2]:
+        # END: for x in test_list:
 
     context = {'latest_question_list': latest_question_list,
-               'test_list': test_list}
+               'test_list': data_rows}
     return render(request, 'connect4/index.html', context)
+
+
+def add_piece_to_board(board, coords, piece):
+    player_x = int(coords.split(',')[0])
+    player_y = int(coords.split(',')[1])
+    board[player_x][player_y] = piece
+
+
+# If this board state already exists in our model, find it and make new_state it.
+# Point our current_state to it, modify current_state's transition + action accordingly.
+# Make our current_state this existing state (assigned to new_state) and continue.
+def existing_board_state(board, current_state, all_states, action, state_id):
+    if repr(board) in all_states:
+        new_state = all_states[repr(board)]
+        # Check to see if it's already been linked from current_state
+        if current_state.next_states.__contains__(new_state):
+            # We need to modify the transition accordingly
+            ind = current_state.next_states.index(new_state)
+            current_state.transitions[ind] = current_state.transitions[ind] + 1
+        else:
+            current_state.add_next_state(new_state)
+            current_state.add_action(action)
+            current_state.add_transition(1)
+    else:
+        new_state = State(board, state_id)
+        new_state.reward = 1
+        current_state.add_next_state(new_state)
+        current_state.add_action(action)
+        current_state.add_transition(1)
+        all_states[repr(new_state.board)] = new_state
+
+    return new_state
 
 
 def detail(request, question_id):
@@ -142,22 +114,6 @@ def vote(request, question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse('connect4:results', args=(question.id,)))
 
-    # TODOs:
-    # - Make it so it logs every other turn
-    # - Setup State object correctly
-    # - Populate state props correctly
-    # - State for "win", "lose", and "tie"
-    # - Work on back propagating...
-    # -
-
-    # State Specs:
-    # - ID
-    # - State: In this case it's the 2d array representing the board
-    # - Array that's an Actions tuple (Action Id, State, Probability)
-    # - Calculate each action as -1, back propagate from goal state
-    # - State Array - an array of all states that link from this
-    # - Probability - an array of the probability corresponding to a state
-
 
 class State:
     def __init__(self, board, num):
@@ -167,12 +123,6 @@ class State:
         self._actions = []
         self._transitions = []
         self._reward = 0
-
-        # def set_id(self, num):
-        # self.id = num
-
-    # def set_reward(self, num):
-    #   self.reward = num
 
     def add_transition(self, num):
         self._transitions.append(num)
